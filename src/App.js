3f397e90ff7fc859dda0f934b820a7307f9a8501
Alex Santana <alexandresntn@yahoo.com>
2021-09-16 12:43:24 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Products from './Products';
 
 function App() {
+  const [formInfo,setFormInfo] =useState({
+    title:null,
+    price:null,
+    description:null
+  })
+
+  const changeHandler=(e)=>{
+    setFormInfo({...formInfo,[e.target.name]:e.target.value
+  })
+}
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    axios.post("http://localhost:8000/api/products",formInfo)
+    .then(res=>{
+      res.json(res)
+    })
+    .catch(err=>console.log(err))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Product Manager</h1>
+        <form onSubmit={(e)=>submitHandler(e)} className="formInfo d-flex flex-column" action="">
+          <input onChange={(e)=>changeHandler(e)} className="mb-3" placeholder="Title" type="text" name="title"/>
+          <input onChange={(e)=>changeHandler(e)} className="mb-3" placeholder="Price" type="number" name="price"/>
+          <input onChange={(e)=>changeHandler(e)} className="mb-3" placeholder="Description" type="text" name="description" id="" />
+          <input className="btn btn-success" type="submit" value="Add" />
+        </form>
+      </div>
+      <Switch>
+        <Route exact path="/route1">
+
+        </Route>
+        <Route exact path="/route2">
+
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Products.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const Products = (props) => {
+//     const [vName,setVName] = useState('');
+//     const history = useHistory();
+//     const {ParamNameFromRoute, otherParam, anotherParam} = useParams();
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Products;
